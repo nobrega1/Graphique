146ce8aeebeff428655cfb3e721dbcd448fd8d17
nobrega1 <56161445+nobrega1@users.noreply.github.com>
2020-04-03 15:59:46 +0200

    test 3

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import * as d3 from 'd3';
 import { geoPath, geoMercator, select, local } from 'd3';
 
-// importer les fichiers GeoJSON
-import localisations from 'Json/map.geojson'
+const data = require('./Json/map.geojson');
 
+
+localPaths = data.features.map(pathCreator);
 
 // la taille de la carte
 const WIDTH = 800
 const HEIGHT = 500
 
-// la projection
-const projection = geoMercator()
-  .fitExtent([[0, 0], [WIDTH, HEIGHT]], localisations)
+const svg = d3.select('body')
+.append('svg')
+.attr('width', WIDTH)
+.attr('height', HEIGHT)
+const DATA = result;
+
+const pathCreator = d3.geoPath().projection(projection)
   
-  // le constructeur d'attribut "d" pour les éléments <path>
-const pathCreator = geoPath().projection(projection)
+const svg = d3.select(container)
 
-
-const svg = select('#carte').append('svg')
-  .attr('width', WIDTH)
-  .attr('height', HEIGHT)
-
-  const groupeRoutes = svg.append('g')
-
-  groupeRoutes.selectAll('path')
-  .data(localisations.positions)
+svg.selectAll('path')
+  .data(cantons.features)
   .enter()
   .append('path')
   .attr('d', pathCreator)
-  .attr('fill', 'none')
-  .attr('stroke', 'lightgray')
-  .attr('stroke-width', 3)
-
 
 
